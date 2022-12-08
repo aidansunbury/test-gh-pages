19b1ac917b1dd7dffff3b2b5e27a47e700d21422
aidansunbury <aidansunbury@gmail.com>
2022-12-07 16:03:28 -0800

    pls

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React or else...
+          Learn React or else... pls fix
         </a>
       </header>
     </div>
